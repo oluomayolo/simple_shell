ae2908a7bca134b3a2a09e87029b432b41243513
Billy Wellington <billierangwani@gmail.com>
2022-11-16 02:00:13 +0200

    Fix betty. It works!

--- testfile.c
+++ testfile.c
@@ -8,11 +8,12 @@
 
 int main(void)
 {
- char name[25];
+	char name[25];
 
- printf("Enter your name: \n");
- scanf("%s", name);
+	printf("Enter your name: \n");
+	scanf("%s", name);
 
- printf("Hello %s\n", name);
- return (0);
+	printf("Hello %s\n", name);
+	return (0);
 }
+
